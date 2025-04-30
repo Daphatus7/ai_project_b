7693cc4960444a14000d025e61931c2361dc2699
Zeming Xia <zemxia@student.unimelb.edu.au>
2025-04-30 08:14:52 +0000

    init board

--- agent/program.py
+++ agent/program.py
@@ -3,6 +3,7 @@
 
 from referee.game import PlayerColor, Coord, Direction, \
     Action, MoveAction, GrowAction
+from referee.game.board import CellState, BOARD_N
     
 
 class Agent:
@@ -16,12 +17,72 @@ class Agent:
         This constructor method runs when the referee instantiates the agent.
         Any setup and/or precomputation should be done here.
         """
+        """ # Print a clear separator for initialization debugging
+        print("\n" + "="*50)
+        print("INITIALIZING AGENT")
+        print("="*50)
+        
+        # Debug referee dictionary content
+        print(f"Referee params: {referee}")
+        
+        # Store our color
         self._color = color
-        match color:
-            case PlayerColor.RED:
-                print("Testing: I am playing as RED")
-            case PlayerColor.BLUE:
-                print("Testing: I am playing as BLUE")
+        print(f"Agent color: {self._color}") """
+        
+        # Initialize with a dictionary-based board representation
+        self._board = {}
+        
+        """ # Setup initial board state
+        print("\nSetting up initial board state...") """
+        
+        # Empty cells
+        for r in range(BOARD_N):
+            for c in range(BOARD_N):
+                self._board[Coord(r, c)] = CellState()
+        
+        # Corner lily pads
+        for r in [0, BOARD_N - 1]:
+            for c in [0, BOARD_N - 1]:
+                self._board[Coord(r, c)] = CellState("LilyPad")
+
+        # Middle row lily pads
+        for r in [1, BOARD_N - 2]:
+            for c in range(1, BOARD_N - 1):
+                self._board[Coord(r, c)] = CellState("LilyPad")
+            
+        # Initial RED and BLUE pieces
+        for c in range(1, BOARD_N - 1):
+            self._board[Coord(0, c)] = CellState(PlayerColor.RED)
+            self._board[Coord(BOARD_N - 1, c)] = CellState(PlayerColor.BLUE)
+        
+        # Set minimax search depth
+        self._search_depth = 3
+            
+        """ # Print the initial board state using ASCII representation
+        self._print_board() """
+
+    def _print_board(self):
+        """
+        Print a text representation of the current board state.
+        """
+        print("  " + " ".join(str(c) for c in range(BOARD_N)))
+        print("  " + "-" * (BOARD_N * 2 - 1))
+        
+        for r in range(BOARD_N):
+            row = f"{r}|"
+            for c in range(BOARD_N):
+                cell_state = self._board[Coord(r, c)].state
+                if cell_state == PlayerColor.RED:
+                    row += "R"
+                elif cell_state == PlayerColor.BLUE:
+                    row += "B"
+                elif cell_state == "LilyPad":
+                    row += "*"
+                else:
+                    row += "."
+                row += " "
+            print(row)
+        
 
     def action(self, **referee: dict) -> Action:
         """
@@ -29,16 +90,10 @@ class Agent:
         to take an action. It must always return an action object. 
         """
 
-
         # Below we have hardcoded two actions to be played depending on whether
         # the agent is playing as BLUE or RED. Obviously this won't work beyond
         # the initial moves of the game, so you should use some game playing
         # technique(s) to determine the best action to take.
-
-
-        #-------decide the action to take-------
-
-
         match self._color:
             case PlayerColor.RED:
                 print("Testing: RED is playing a MOVE action")
@@ -55,8 +110,6 @@ class Agent:
         This method is called by the referee after a player has taken their
         turn. You should use it to update the agent's internal game state. 
         """
-
-        # --------- This is called after the referee validates the action ---------
 
         # There are two possible action types: MOVE and GROW. Below we check
         # which type of action was played and print out the details of the
