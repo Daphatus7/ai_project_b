8656976fce13eb617d774dafe9e545eaa896ce28
peiyuw2 <peiyuw08@gmail.com>
2025-05-05 10:58:56 +1000

    simple evaluation function modified

--- agent/program.py
+++ agent/program.py
@@ -327,12 +327,12 @@ class MinMaxSearch:
         return grow_tiles
 
     WEIGHTS = [
+        5,
         10,
-        20,
+        15,
         30,
         40,
         50,
-        60,
         70,
         100
     ]
