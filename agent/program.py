f48163d41b6c2054bf6cce84988a6a7b0a30cf6b
peiyuw2 <peiyuw08@gmail.com>
2025-05-08 10:15:36 +1000

    fix error in the best_move method

--- agent/program.py
+++ agent/program.py
@@ -55,11 +55,6 @@ class Agent:
         """ # Setup initial board state
         print("\nSetting up initial board state...") """
 
-        # # Empty cells - don't need empty cells
-        # for r in range(BOARD_N):
-        #     for c in range(BOARD_N):
-        #         self._board[Coord(r, c)] = CellState()
-
         # Corner lily pads
         for r in [0, BOARD_N - 1]:
             for c in [0, BOARD_N - 1]:
@@ -76,7 +71,7 @@ class Agent:
             self._board[Coord(BOARD_N - 1, c)] = 'b'
 
         # Set minimax search depth
-        self._search_depth = 5 # error when search depth is 1
+        self._search_depth = 6 # error when search depth is 1
         self.__brain = MinMaxSearch(self._board, self._search_depth, self.__color)
 
         #self.__print_board()
@@ -108,7 +103,7 @@ class Agent:
     def action(self, **referee: dict) -> Action:
         """
         This method is called by the referee each time it is the agent's turn
-        to take an action. It must always return an action object. 
+        to take an action. It must always return an action object.
         """
         # Below we have hardcoded two actions to be played depending on whether
         # the agent is playing as BLUE or RED. Obviously this won't work beyond
@@ -123,7 +118,7 @@ class Agent:
     def update(self, color: PlayerColor, action: Action, **referee: dict):
         """
         This method is called by the referee after a player has taken their
-        turn. You should use it to update the agent's internal game state. 
+        turn. You should use it to update the agent's internal game state.
         """
         print("referee update actions",  action)
         print("----------Before-------------")
@@ -210,7 +205,7 @@ def terminal_test(board: dict[Coord, str], depth) -> bool:
     """
     check if satisfier terminal condition
     1. has reached maximum depth
-    2.
+    2.all frogs reached goal row
     """
     # if has explored the maximum depth
     if depth == 0:
@@ -326,83 +321,44 @@ class MinMaxSearch:
                 grow_tiles.append(Coord(new_row, new_col))
         return grow_tiles
 
-    WEIGHTS = [
-        5,
-        10,
-        15,
-        30,
-        40,
-        50,
-        70,
-        100
-    ]
+    WEIGHTS = [5, 10, 15, 30, 40, 50, 70, 100]
+
 
     def evaluation_function(self, curr_board: dict[Coord, str], my_color: PlayerColor) -> float:
         """
         Evaluate the board state. Higher values are better for the player.
         """
-        """
-            # 1 distance : closer to the goal the higher points - where the enemy 
-            # 2 movements : c
-            # 3 grow count
-            # opponent
-                # 1.
-        """
-
-
-        frog_color_char = None
-        opponent_color_char = None
+        frog_color = None
+        opponent_color = None
         if my_color == PlayerColor.RED:
-            frog_color_char = 'r'
-            opponent_color_char = 'b'
+            frog_color = 'r'
+            opponent_color = 'b'
         elif my_color == PlayerColor.BLUE:
-            frog_color_char = 'b'
-            opponent_color_char = 'r'
+            frog_color = 'b'
+            opponent_color = 'r'
         else:
             assert False
+
         goal_row = 0 if my_color == PlayerColor.RED else BOARD_N - 1
 
-        def get_all_frog(color_char : str) -> list[Coord]:
+        def get_all_frogs(color: str) -> list[Coord]:
             frogs = []
             for cell, state in list(curr_board.items()):
-                if state == color_char:
+                if state == color:
                     frogs.append(cell)
             return frogs
 
-        def evaluate_distance_score(frogs : list[Coord]) -> float:
+        def evaluate_distance_score(frogs: list[Coord]) -> float:
             score = 0
             for frog in frogs:
                 distance = abs(goal_row - frog.r)
                 score += self.WEIGHTS[distance]
             return score
 
-        def evaluate_opponent_distance_score(frogs : list[Coord]) -> float:
-            score = 0
-            for frog in frogs:
-                distance = abs(goal_row - frog.r)
-                score += self.WEIGHTS[distance]
-            return score
+        total_score = evaluate_distance_score(get_all_frogs(frog_color)) - \
+                      evaluate_distance_score(get_all_frogs(opponent_color))
 
-        def evaluate_space_score(frog_color : PlayerColor) -> float:
-            """
-            The more freedom the player has the higher the score
-            """
-            score = 0
-            return score
-
-        def evaluate_opponent_space_score(frogs : list[Coord]) -> float:
-            score = 0
-            return score
-
-        total_score = evaluate_distance_score(get_all_frog(
-            frog_color_char,
-        )) - evaluate_opponent_distance_score(
-            get_all_frog(
-                opponent_color_char,
-            )
-        )
         return total_score
-
 
     def temp_jump_only_consider_one(self, color: PlayerColor) -> list[Direction]:
         pass
@@ -555,6 +511,33 @@ class MinMaxSearch:
         2. we estimate the impact of next move
         3. then we conclude if the next move is the best
         """
+        goal_row = 0 if my_color == PlayerColor.BLUE else BOARD_N - 1
+
+        # 1) instant win, simple steps and jumps
+        for frog in get_frog_coords(self.board, my_color):
+            for direction in get_possible_directions(my_color):
+                # compute candidate step
+                new_r = frog.r + direction.r
+                new_c = frog.c + direction.c
+
+                if not self.is_on_board(new_c, new_r):
+                    continue
+                # straight move to the goal row
+                if is_valid_move(self.board, Coord(new_r, new_c)):
+                    if new_r == goal_row:
+                        return MoveAction(frog, direction)
+
+                # check jump possibility
+                jump_start = Coord(new_r, new_c)
+
+                if self.can_jump(self.board, jump_start, direction):
+                    for jump in self.get_all_possible_jumps(jump_start, self.board, my_color):
+                        landing_r, landing_c = jump.coord.r, jump.coord.c
+                        for d in jump.directions:
+                            landing_r += d.r
+                            landing_c += d.c
+                        if landing_r == goal_row:
+                            return jump
 
         # min value
         max_value = float('-inf')
@@ -584,26 +567,30 @@ class MinMaxSearch:
                 move_c = frog_location.c + direction.c
                 if not self.is_on_board(move_c, move_r):
                     continue
-                move = frog_location + direction
                 # if the next cell is a lilypad
-                if is_valid_move(self.board, move):
+                if is_valid_move(self.board, Coord(move_r, move_c)):
                     # Create a move action with a single direction
                     move_action = MoveAction(frog_location, direction)
-                    value = self.min_max_value(self.apply_action(self.board.copy(), move_action, self.color),
-                                               opposite_color(self.color),
-                                               explore_depth,
-                                               alpha,
-                                               beta,
-                                               False)
+                    value = self.min_max_value(
+                        self.apply_action(self.board.copy(), move_action, my_color),
+                               opposite_color(my_color),
+                               explore_depth,
+                               alpha,
+                               beta,
+                               False)
                     if value > max_value:
                         max_value = value
                         best_move = move_action
+                    alpha = max(alpha, value)
+                    if beta <= alpha:
+                        break
 
-                elif self.can_jump(self.board, move, direction):
+                jump_start = Coord(move_r, move_c)
+                if self.can_jump(self.board, jump_start, direction):
                     # need to check every possible jumps
-                    for jump in self.get_all_possible_jumps(move, self.board, self.color):
-                        value = self.min_max_value(self.apply_action(self.board.copy(), jump, self.color),
-                                                   opposite_color(self.color),
+                    for jump in self.get_all_possible_jumps(jump_start, self.board, my_color):
+                        value = self.min_max_value(self.apply_action(self.board.copy(), jump, my_color),
+                                                   opposite_color(my_color),
                                                    explore_depth,
                                                    alpha,
                                                    beta,
@@ -611,8 +598,9 @@ class MinMaxSearch:
                         if value > max_value:
                             max_value = value
                             best_move = jump
-                if beta <= alpha:
-                    break
+                        alpha = max(alpha, value)
+                        if beta <= alpha:
+                            break
             if beta <= alpha:
                 break
         print("best move", best_move)
